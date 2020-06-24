522aaee50b55cd041c3a4d6c8c3c2a7352bec42f
Nicolas Rodriguez <nicoladmin@free.fr>
2020-06-24 04:24:21 +0200

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,24 @@
-FROM ruby:2.4.2-onbuild
+FROM ruby:2.7.1-slim-buster
 MAINTAINER nicoladmin@free.fr
 
 # Declare some environment variables
 ENV DOCKER=true \
     RACK_ENV=production
+
+# Install dependencies
+RUN apt-get update && apt-get -y upgrade && apt-get -y install build-essential
+
+# Create application directory
+RUN /bin/sh -c "mkdir -p /usr/src/app"
+WORKDIR /usr/src/app
+
+# Install gems
+COPY Gemfile /usr/src/app/
+COPY Gemfile.lock /usr/src/app/
+RUN bundle install
+
+# Install application
+COPY . /usr/src/app
 
 # Copy entrypoint script. This is a init script but
 # just for apt-cacher-ng as there is not init system
--- config/puma.rb
+++ config/puma.rb
@@ -13,11 +13,11 @@ if ENV['RACK_ENV'] == 'production' || ENV['RACK_ENV'] == 'staging'
     bind "tcp://#{ENV.fetch('LISTEN', '0.0.0.0')}:#{ENV.fetch('PORT', 5000)}"
   else
     bind "unix://#{File.join(Dir.pwd, 'tmp', 'sockets', 'puma.sock')}"
-  end
 
-  state_path           File.join(Dir.pwd, 'tmp', 'sockets', 'puma.state')
-  activate_control_app "unix://#{File.join(Dir.pwd, 'tmp', 'sockets', 'pumactl.sock')}"
-  stdout_redirect      File.join(Dir.pwd, 'log', 'puma.stdout.log'), File.join(Dir.pwd, 'log', 'puma.stderr.log')
+    state_path           File.join(Dir.pwd, 'tmp', 'sockets', 'puma.state')
+    activate_control_app "unix://#{File.join(Dir.pwd, 'tmp', 'sockets', 'pumactl.sock')}"
+    stdout_redirect      File.join(Dir.pwd, 'log', 'puma.stdout.log'), File.join(Dir.pwd, 'log', 'puma.stderr.log')
+  end
 else
   bind "tcp://#{ENV.fetch('LISTEN', '127.0.0.1')}:#{ENV.fetch('PORT', 5000)}"
 end
