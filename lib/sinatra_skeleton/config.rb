2aedf00cfe98e5748ce9bad7b10cc446fafe3cad
Nicolas Rodriguez <nico@nicoladmin.fr>
2021-05-05 02:17:18 +0200

    Add check on Postgres DB

--- lib/sinatra_skeleton/config.rb
+++ lib/sinatra_skeleton/config.rb
@@ -15,9 +15,14 @@ module SinatraSkeleton
         hostname:   Socket.gethostname,
         ip_address: Socket.ip_address_list.detect { |intf| intf.ipv4_private? }.ip_address,
         user:       Etc.getlogin,
+        postgres:   postgres_check,
         env:        ENV.to_h,
       }
     end
 
+    def self.postgres_check
+      conn = PG.connect(host: ENV['POSTGRES_DB_HOST'], port: ENV['POSTGRES_DB_PORT'], user: ENV['POSTGRES_DB_USER'], password: ENV['POSTGRES_DB_PASS'], dbname: ENV['POSTGRES_DB_NAME'])
+      conn.exec( "SELECT * FROM pg_stat_activity" ).to_a.select { |r| r['usename'] == ENV['POSTGRES_DB_USER'] }
+    end
   end
 end
