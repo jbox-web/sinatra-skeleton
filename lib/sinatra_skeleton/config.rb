a5f142a035fb139f5b7bbab1fd71fd4e4d2a649a
Nicolas Rodriguez <nico@nicoladmin.fr>
2021-05-05 03:05:30 +0200

    Rescue from all exceptions in DB checks

--- lib/sinatra_skeleton/config.rb
+++ lib/sinatra_skeleton/config.rb
@@ -25,16 +25,22 @@ module SinatraSkeleton
     def self.postgres_check
       conn = PG.connect(host: ENV['POSTGRES_DB_HOST'], port: ENV['POSTGRES_DB_PORT'], user: ENV['POSTGRES_DB_USER'], password: ENV['POSTGRES_DB_PASS'], dbname: ENV['POSTGRES_DB_NAME'])
       conn.exec( "SELECT * FROM pg_stat_activity" ).to_a.select { |r| r['usename'] == ENV['POSTGRES_DB_USER'] }
+    rescue => e
+      e.message
     end
 
     def self.mysql_check
       conn = Mysql2::Client.new(host: ENV['MYSQL_DB_HOST'], port: ENV['MYSQL_DB_PORT'], username: ENV['MYSQL_DB_USER'], password: ENV['MYSQL_DB_PASS'], database: ENV['MYSQL_DB_NAME'])
       conn.query('SELECT * from mysql.user').to_a.select { |r| r['User'] == ENV['MYSQL_DB_USER'] }
+    rescue => e
+      e.message
     end
 
     def self.redis_check
       conn = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'])
       conn.incr('hits')
+    rescue => e
+      e.message
     end
   end
 end
